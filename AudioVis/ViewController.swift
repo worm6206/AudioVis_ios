b9f19ff2b9ab9e4f6e84a6b94b78a8d05ec030c1
Kevin Yen <worm6206@gmail.com>
2016-03-29 20:40:30 -0400

    Initial Commit

---
+++ AudioVis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioVis
+//
+//  Created by Kevin Yen on 3/29/16.
+//  Copyright © 2016 Kevin Yen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
