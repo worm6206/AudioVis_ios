a8f6f243ad066cd362683fe42e77cdd72cd67672
Kevin Yen <worm6206@gmail.com>
2016-03-29 22:35:40 -0400

    day1

--- AudioVis/ViewController.swift
+++ AudioVis/ViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Test")
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        nameLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    @IBAction func submitButton(sender: AnyObject) {
+        nameLabel.text = "HOLA!"
+    }
+    
 }
 
